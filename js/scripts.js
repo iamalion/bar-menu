e350c8846d7078b05994d446fb8cd576dc26d895
Lindsay Warr <iamalion@gmail.com>
2023-06-01 09:05:41 -0700

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    const form = document.querySelector("form");
+    form.onsubmit = function(event) {
+      event.preventDefault();
+  
+      let drinkMenu = document.getElementById("drinks");
+      drinkMenu.setAttribute("class", "hidden");
+      let under21Message = document.getElementById("under-21");
+      under21Message.setAttribute("class", "hidden");
+  
+      const age = parseInt(document.querySelector("input#age").value);
+  
+      if (age > 21) {  // updated to check if age is greater than 21
+        drinkMenu.removeAttribute("class");
+      } else if (age === 21) { // new else if statement
+        window.alert("Have some fun, you’re just 21!");
+        drinkMenu.removeAttribute("class");
+      } else {
+        under21Message.removeAttribute("class");
+      }
+    };
+  };
